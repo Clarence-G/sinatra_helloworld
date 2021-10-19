050972e101a88367b23d4377dff95dfedcae78f4
Clarence <yoououyo@qq.com>
2021-10-19 22:10:47 +0800

    modify app

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
     get '/' do
-          "<!DOCTYPE html><html><head></head><body><h1>GoodGood Study, DayDay up</h1></body></html>"
+          "<!DOCTYPE html><html><head></head><body><h1>凯锐牛啤！🐮🍺</h1></body></html>"
             end
 end
 
